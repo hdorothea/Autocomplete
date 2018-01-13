34b4ab36f5dfee5fe3b4a7c20b9566cbdfa06449
hdorothea <dorothea.hormann@gmail.com>
2018-01-12 19:43:06 -0800

    Add controler docs

--- app/controller.js
+++ app/controller.js
@@ -5,7 +5,9 @@ export default class Controller {
     this.suggestionContainerView = suggestionContainerView;
     this.model = model;
   }
-
+  /**
+   * Bind eventlisteners and callbacks
+   */
   run() {
     this.queryInputView.bindInputChange(this.updateQuery.bind(this));
     this.suggestionContainerView.bindOutSideClick(document, this.resetSuggestions.bind(this));
@@ -18,36 +20,52 @@ export default class Controller {
     this.suggestionView.bindMouse('mouseout', this.deactivateSuggestion.bind(this));
     this.suggestionView.bindClick(this.acceptActiveSuggestion.bind(this));
   }
-
+  /**
+   * Reset the suggestions
+   */
   resetSuggestions() {
     this.model.resetSuggestions(this.suggestionView.update.bind(this.suggestionView));
   }
-
+  /**
+   * Update the query to newQuery
+   * @param  {string} newQuery
+   */
   updateQuery(newQuery) {
     this.model.updateQuery(newQuery, this.suggestionView.update.bind(this.suggestionView));
   }
-
+  /**
+   * Activate the next/previous suggestion
+   * @param  {boolean} dec=false Should I activate the previous suggestion default is next
+   */
   incrementActiveSuggestion(dec = false) {
     this.model.incrementActiveSuggestion(
       dec,
       this.suggestionView.switchActive.bind(this.suggestionView)
     );
   }
-
+  /**
+   * Deactivate a suggestion
+   * @param  {string} suggestion
+   */
   deactivateSuggestion(suggestion) {
     this.model.deactivateSuggestion(
       suggestion,
       this.suggestionView.unmarkActive.bind(this.suggestionView)
     );
   }
-
+  /**
+   * Activate a new suggestion either by index or text content
+   * @param  {(string|number)} activeSuggestion
+   */
   activateSuggestion(activeSuggestion) {
     this.model.activateSuggestion(
       activeSuggestion,
       this.suggestionView.switchActive.bind(this.suggestionView)
     );
   }
-
+  /**
+   * Accept the currently active suggestion as a query
+   */
   acceptActiveSuggestion() {
     if (this.model.activeSuggestionI !== null) {
       this.model.acceptActiveSuggestion(' ', (newQuery, newSuggestions) => {
@@ -56,12 +74,14 @@ export default class Controller {
       });
     }
   }
-
+  /**
+   * Accept the active suggestion if there is one otherwise submit it
+   */
   submitOrAcceptActiveSuggestion() {
     if (this.model.activeSuggestionI !== null) {
       this.acceptActiveSuggestion();
     } else {
-      this.model.submit();
+      // logic for submitting the query
     }
   }
 }
