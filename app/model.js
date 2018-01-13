886efab1237f5acf5f99c7e66c60b303878834c8
hdorothea <dorothea.hormann@gmail.com>
2018-01-12 20:01:57 -0800

    Add model docs

--- app/model.js
+++ app/model.js
@@ -4,8 +4,12 @@ export default class Model {
   setQuery(query) {
     this.query = query;
   }
-
-  setSuggestions(suggestions) {
+  /**
+   * Fetch the suggestions for the last word from the backend. Update suggestions
+   * and related properties to reflect the new suggestions
+   * @param  {Function} callback Called with the new suggestions and difference slices
+   * when the suggestions and difference slices have been updated
+   */
     this.suggestions = suggestions;
   }
 
@@ -13,7 +17,12 @@ export default class Model {
     const suggestions = await getLastWordAutoCompleteSuggestionsFakeBackend(this.query, true);
     this.setSuggestions(suggestions);
   }
-
+  /**
+   * Update query and suggestions to reflect the new query
+   * @param  {!string} newQuery
+   * @param  {Function} callback Called with the new suggestions and difference slices
+   * when the query and suggestions have been completed
+   */
   async updateQuery(newQuery, callback) {
     this.setQuery(newQuery);
     await this.updateSuggestions();
@@ -26,7 +35,12 @@ export default class Model {
   setActiveSuggestionI(activeSuggestionI) {
     this.activeSuggestionI = activeSuggestionI;
   }
-
+  /**
+   * Reset the currently activeSuggestion if it is the same as suggestion
+   * @param  {string} suggestion
+   * @param  {Function} callback Called with the previously active suggestion
+   * if the active suggestion was reset
+   */
   deactivateSuggestion(suggestion, callback) {
     if (this.suggestions[this.activeSuggestionI] === suggestion) {
       const previouslyActiveSuggestion = this.suggestions[this.activeSuggestionI];
@@ -36,11 +50,10 @@ export default class Model {
       }
     }
   }
-
-  submit() {
-    console.log(`You submitted ${this.query}`);
-  }
-
+  /**
+   * Reset suggestions and the current activeSuggestion accordingly
+   * @param  {Function} callback Called with the new suggestions when the suggestions are reset
+   */
   resetSuggestions(callback) {
     this.setSuggestions([]);
     this.setActiveSuggestionI(null);
@@ -48,7 +61,12 @@ export default class Model {
       callback(this.suggestions);
     }
   }
-
+  /**
+   * Activates the next(in a circular manner) suggestion
+   * @param  {boolean} dec=false Should it activate the previous suggestion default is next
+   * @param  {Function} callback Called with the previous and next
+   * suggestion once the next suggestion has been activated
+   */
   incrementActiveSuggestion(dec = false, callback) {
     const newActiveSuggestionI = getNextIndexCircular(
       this.suggestions,
@@ -58,7 +76,13 @@ export default class Model {
 
     this.activateSuggestion(newActiveSuggestionI, callback);
   }
-
+  /**
+   * Accept the currently active suggestion as the new Query
+   * @param  {string} [postFix=""] String to be added to the end of
+   * the query once the suggestion is accepted
+   * @param  {Function} callback Called with the new suggestions
+   * once the query and the suggestions are updated
+   */
   acceptActiveSuggestion(postFix = '', callback) {
     this.setQuery(this.suggestions[this.activeSuggestionI] + postFix);
     this.resetSuggestions();
@@ -67,6 +91,13 @@ export default class Model {
     }
   }
 
+  /**
+   * Set a new active suggestion
+   * @param  {(number | string)} suggestion New Suggestion index or one of the
+   * strings inside suggestions
+   * @param  {Function} callback Called once the new active suggestion index has been set
+   * with the previously and newly active suggestion
+   */
   activateSuggestion(suggestion, callback) {
     const previouslyActiveSuggestion = this.suggestions[this.activeSuggestionI];
 
